bb27392718e31e5b28abb7dc6a0f826eaf9f1693
footballer2021 <soccerzuki2013@gmail.com>
2024-01-14 17:59:50 +0900

    amend

--- src/PointInput.tsx
+++ src/PointInput.tsx
@@ -4,13 +4,13 @@ type StateType = {
     icoca:number
 }
 
-const PointInput = ({name,state,dispatch}:{name:string,state:StateType,dispatch:React.Dispatch<React.SetStateAction<StateType>>}) => {
+const PointInput = ({name,state,dispatch}:{name:keyof StateType,state:StateType,dispatch:React.Dispatch<React.SetStateAction<StateType>>}) => {
   return (
     <div>
         {name}:<input 
             type="number"
             name={name}
-            value={state[name as keyof object]}
+            value={state[name]}
             onChange={(e) => dispatch({...state, [e.target.name]:e.target.value})}
         />
     </div>
